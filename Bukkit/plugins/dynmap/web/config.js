9f19449dc9f7a9e58e4a0f2024076af3c1ca8205
Emil Kristensen <e@ekristensen.dk>
2011-04-23 14:10:13 +0200

    tmp removed some files

--- Bukkit/plugins/dynmap/web/config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-        tileUrl:     'tiles/',
-        updateUrl:   'up/',                             // For Apache and lighttpd
-//      updateUrl:   'up.aspx?path=',                   // For IIS
-		tileWidth: 128,
-		tileHeight: 128
-};
--- Bukkit/server.sh
+++
@@ -1,1 +0,0 @@
-java -server -Xmx3000M -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -jar craftbukkit-556.jar
